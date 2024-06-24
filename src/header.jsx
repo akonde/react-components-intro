9ec3e6551fa2f5dfe4a04bc83c32c17e76ef26a3
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-06-24 06:24:23 +0100

    completed!

---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+import NavLeft from "./nav_left_menu";
+import NavRight from "./nav_right_menu";
+function Header() {
+  return (
+    <header>
+      <NavLeft />
+      <NavRight />
+    </header>
+  );
+}
+export default Header;
